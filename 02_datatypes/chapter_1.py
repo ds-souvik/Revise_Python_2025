e37637a556fb25a5a731f8e50b1426d0f44010bd
Souvik Ganguly <souvik.ganguly.ds@gmail.com>
2025-08-13 22:39:40 +0530

    Add 00_basics 01_virtual 02_datatypes/chapter_1.py

---
+++ 02_datatypes/chapter_1.py
@@ -0,0 +1,28 @@
+'''Checking the mutability property of integer'''
+sugar_amount = 2 
+
+print(f"Initial sugar amount: {sugar_amount}")
+
+print(f"Initial sugar amount id: {id(sugar_amount)}")
+
+sugar_amount = 12 
+print(f"New sugar amount: {sugar_amount}")
+
+print(f"New sugar amount id: {id(sugar_amount)}")
+
+'''Checking the mutability property of set'''
+
+print("******CHANGE DATATYPE: SET******")
+
+spice_mix = set() 
+
+print(f"Initial spice mix : {spice_mix}")
+
+print(f"Initial spice mix id: {id(spice_mix)}")
+
+spice_mix.add("Ginger")
+spice_mix.add("cardamom")
+print(f"New spice mix : {spice_mix}")
+
+print(f"New spice mix id: {id(spice_mix)}")
+
