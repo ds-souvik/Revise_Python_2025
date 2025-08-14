38e58147ddd4da7f9f2517d4b0b7235dcc0d3900
Souvik Ganguly <souvik.ganguly.ds@gmail.com>
2025-08-14 16:21:44 +0530

    Add chapter_2.py and chapter_3.py that has basic practice on numbers and strings

---
+++ 02_datatypes/chapter_2.py
@@ -0,0 +1,52 @@
+'''Operations with numbers'''
+
+'''1. Operations with Integers'''
+
+souvik_age = 30
+
+sayli_age = 31
+
+print(f'Couples age: {souvik_age + sayli_age}')
+
+print(f"Diference between couple's age : {sayli_age - souvik_age}")
+
+print(f'Product of Couples age: {souvik_age * sayli_age}')
+
+print(f'Average of couples age: {(sayli_age + souvik_age) / 2}')
+
+print(f'Integer Average of couples age: {(sayli_age + souvik_age) // 2}')
+
+print(f"Remainder when sayli's age divided by souvik's age: {sayli_age % souvik_age}") 
+
+print(f'2 to the power 3: {2**3}')
+
+'''2. Operations with Boolean'''
+
+is_happy = True
+no_of_people = 2 
+
+print(f"Is the couple happy? : {is_happy}")
+
+print(f"Operation between integer and boolean: {is_happy + no_of_people}")
+
+couple_fights_sometimes_1 = 0
+couple_fights_sometimes_2 = 10
+couple_fights_sometimes_3 = 2
+couple_fights_sometimes_4 = 1 
+couple_fights_sometimes_5 = 11
+couple_fights_sometimes_6 = -1
+couple_fights_sometimes_7 = None
+
+print(f"Does the couple foght sometimes_1? : {bool(couple_fights_sometimes_1)}")
+
+print(f"Does the couple foght sometimes_2? : {bool(couple_fights_sometimes_2)}")
+
+print(f"Does the couple foght sometimes_3? : {bool(couple_fights_sometimes_3)}")
+
+print(f"Does the couple foght sometimes_4? : {bool(couple_fights_sometimes_4)}")
+
+print(f"Does the couple foght sometimes_5? : {bool(couple_fights_sometimes_5)}") 
+
+print(f"Does the couple foght sometimes_6? : {bool(couple_fights_sometimes_6)}")
+
+print(f"Does the couple foght sometimes_7? : {bool(couple_fights_sometimes_7)}")
