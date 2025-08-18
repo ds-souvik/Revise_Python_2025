977e1d170848be6ef71fcad749ae5c4fd65c1ba4
Souvik Ganguly <souvik.ganguly.ds@gmail.com>
2025-08-18 23:12:46 +0530

    Add a program using Ternary operator: Delivery fees waiver system

---
+++ 03_conditionals/8_delivery_fees_waiver_system.py
@@ -0,0 +1,15 @@
+""" DELIVERY FEES WAIVER SYSTEM:
+You run an online tea store. 
+If the order amount is more than 300 rs:
+    Delivery is free
+Otherwise:
+    It costs 30 rs.
+
+Task:
+1. Input order amount
+2. Use ternary operator to decide delivery fee
+"""
+#ternary operator
+order_input = int(input("Order amount: "))
+print(f"{'Congratulations! Free Delivery' if order_input > 300 else ('Sorry, Invalid input. Please try again' 
+                                                     if order_input < 0 else 'Delivery charges: 30 rs')}")
