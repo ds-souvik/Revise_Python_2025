9b002e486b887a62c47888a3a1ad0c36deb82455
Souvik Ganguly <souvik.ganguly.ds@gmail.com>
2025-08-21 07:14:36 +0530

    Add batch chai preparation program

---
+++ 04_loops/02_batch_chai_preparation.py
@@ -0,0 +1,9 @@
+"""A chai shop makes tea in batcges every 15 minutes.
+You want to simulate 4 batches.
+Task:
+    1. Use range() to simulate batch numbers.
+    2. Print: "Preparing chai for batch #[number]"""
+
+
+for batch in range(1,5):
+    print(f"Preparing chai for batch #{batch}")
