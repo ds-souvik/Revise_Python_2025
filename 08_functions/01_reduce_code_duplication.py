2b93ee99cdef799c340f073c6dd3bef19804f39c
Souvik Ganguly <souvik.ganguly.ds@gmail.com>
2025-09-05 17:55:49 +0530

    Add a code to understand improving readability using functions.

--- 08_functions/01_reduce_code_duplication.py
+++ 08_functions/01_reduce_code_duplication.py
@@ -5,13 +5,13 @@ Task:
     1. Write a function print_order(name, chai_type)
     2. Call it multiple times for different customers."""
 
-def print_order(name, chai_type):
+def print_order(name, chai_type): #variables here are called parameters
     #check if the variables are populated and if the variables are of string datatype
     if name and chai_type and isinstance(name, str) and isinstance(chai_type, str):
         return f"{name} ordered {chai_type}"
     else:
         return "Incomplete order"
 
-print(print_order("Souvik", "Ginger tea"))
+print(print_order("Souvik", "Ginger tea")) #variables here are called arguments
 print(print_order("", "Lemon team"))
 print(print_order(123, "Ginger tea"))
